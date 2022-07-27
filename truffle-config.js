aaec1425866e0eb626b7580da249d866805492f0
sheng yeong <chia.shengyeong@gmail.com>
2022-07-27 15:24:48 +0800

    added live mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -24,7 +24,23 @@ module.exports = {
       gasPrice: 20000000000, // how much we are willing to spent for unit of gas
       confirmations: 2, // number of blocks to wait between deployment
       timeoutBlocks: 200 // number of blocks before deployment times out
-    }
+    },
+    live: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://mainnet.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 1,
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent, and check gas prices
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas, and check gas prices
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200, // number of blocks before deployment times out
+      skipDryRun: true
+    }    
   },
   compilers: {
     solc: {
