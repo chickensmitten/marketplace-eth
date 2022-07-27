b0989bb1f01cc7968e457b5232a3ee641f1be9f8
sheng yeong <chia.shengyeong@gmail.com>
2022-07-27 11:12:00 +0800

    added code to migrate to truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,34 @@
 
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const keys = require("./keys.json")
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
     development: {
-     host: "127.0.0.1",  
-     port: 7545,            
-     network_id: "*",      
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*",
     },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 3,
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
+    }
   },
   compilers: {
     solc: {
-      version: "0.8.12",    
+      version: "0.8.4",
     }
-  },
-
-};
+  }
+}
